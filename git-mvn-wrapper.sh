fa15ffa5433098af689496ddae73a496ab644356
Andreas Kohn <andreas.kohn@gmail.com>
2015-07-21 01:32:00 +0200

    Add support for MacOS X (10.10)
    * xargs -r: GNU extension, reimplemented using a shell function
    * sed -z: GNU extension, reimplemented by piping through echo (removing
      the newlines) and tr (adding the ',')
    * xargs -I{}: reimplemented using a for-loop, to avoid hitting
      the 255-byte limitation for eacho argument of xargs(1)
    
    While there: replace the grep-parsing by a filtering for-loop, which
    as a side-effect fixes command line arguments with quotes due to the use
    of eval.

--- git-mvn-wrapper.sh
+++ git-mvn-wrapper.sh
@@ -11,13 +11,44 @@
 
 set -e
 
-if ! echo "$*" | grep -qE '(^|\s)-O(\s|$)'; then
+# Replacement for GNU xargs -r
+maybe_xargs() {
+  _in=
+  while read _line; do
+    _in="${_line} ${_in}"
+  done
+  if [ -n "${_in}" ]; then
+    "$@" ${_in}
+  fi
+}
+
+find_module() {
+  NDIR=$(dirname "$1")
+  DIR="."
+  while [ ! "$NDIR" = "$DIR" ] && [ ! -f "$NDIR/pom.xml" ]; do
+    DIR="$NDIR"
+    NDIR=$(dirname "$DIR")
+  done
+  echo "$NDIR"
+}
+
+real_args=
+optimize=
+while [ $# -gt 0 ]; do
+  if [ "$1" = "-O" ]; then
+    optimize=true
+  else
+    real_args="${real_args} \"$1\""
+  fi
+  shift
+done
+
+if [ -z "${optimize}" ]; then
   # Regular Maven execution requested. Pass on all arguments as-is.
-  mvn "$@"
+  eval mvn ${real_args}
 else
   # Optimized Maven execution requested. Drop the custom flag.
-  ARGS=$(echo "$*" | sed -r 's,(^|\s)-O(\s|$),\1\2,')
-  set -- $ARGS
+  set -- ${real_args}
 
   if ! git rev-parse --is-inside-git-dir > /dev/null 2>&1; then
     # This is not a Git repository; can't optimize the build.
@@ -29,18 +60,12 @@ else
     # module to which the file belongs. Remove duplicates and join the
     # remaining module directory names with commas. Lastly, instruct Maven to
     # only build modified modules and their dependents.
-    git ls-files --modified \
-      | xargs -I{} sh -c '
-          NDIR=$(dirname "{}")
-          DIR="."
-          while [ ! "$NDIR" = "$DIR" ] && [ ! -f "$NDIR/pom.xml" ]; do
-            DIR="$NDIR"
-            NDIR=$(dirname "$DIR")
-          done
-          echo "$NDIR"
-      ' \
+    for _file in `git ls-files --modified`; do
+      find_module "${_file}"
+    done \
       | sort -u \
-      | sed -z 's/\n/,/g' \
-      | xargs -r mvn $@ -amd -pl
+      | xargs echo \
+      | tr ' ' ',' \
+      | eval maybe_xargs mvn $@ -amd -pl
   fi
 fi
